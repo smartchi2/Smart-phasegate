2b4196cac7df2515eb8e1daceb55a04c52b8138f
smartchi2 <chismart76@gmail.com>
2024-05-13 17:18:31 +0100

    update

--- phase 1/day 7/java/MenstrualApp.java
+++ phase 1/day 7/java/MenstrualApp.java
@@ -9,23 +9,53 @@ public class MenstrualApp{
 			and ends for us to be able to continue with you request..
 							""" );
 
-			int cycleLenght = 0;
-			int periodLenght = 0;
+			int cycleLength = 0;
+			int periodLength = 0;
 			
 
 			System.out.println("=".repeat(100));
 			System.out.println("""
-				Please enter some info about your below menstrual
-				 cycle it Should be entered in days
-							""");
+				Please enter some info about your menstrual 
+		              		cycle, the lenght, the period duration and it 
+				Should be entered in  days.Check below to 
+				see options.""");
 			System.out.println("=".repeat(100));
 
 			System.out.println("""
-			Please enter the average length of your mentrual 
+			Please enter the average length of your menstrual 
 			cycle that you can remember in days: """);
-			cycleLenght = input.nextInt();
+			cycleLength = input.nextInt();
 
-			//System.out.println("
+			System.out.println("""
+				Please Enter the average duration
+				 of your period that you can remember in days:""");
+				periodLength = input.nextInt();
+
+			System.out.println("=".repeat(100));
+			System.out.printf("""
+				We at  Smart Female Health central says 
+				Thank you for completing the steps provided by us. 
+				Hold on as we process your result
+						""");
+			int cycleDuration = cycleLength - periodLength;
+			int fertilePhaseStart = cycleDuration / 14;
+
+			System.out.println("=".repeat(100));
+			System.out.println("the fertile phase starts 14 days before the next period");
+			int fertilePhaseEnd = cycleDuration - 10;
+
+			System.out.println("=".repeat(100));
+			System.out.println("fertile phase ends 10 days before the next period");
+			
+			System.out.println("=".repeat(100));
+			System.out.println("""
+			From our research at Smart Female Health Central.
+			Here are your menstrual cycle statistic:""");
+			System.out.println("Average length of menstrual cycle:\t" +cycleLength   + "\tdays\t");
+			System.out.println("Average duration of period:\t" +periodLength   + " \tdays\t");
+			System.out.println("Average length of cycle excluding period:\t " + cycleDuration   + "\tdays\t");
+			System.out.println("Fertile phase (potential ovulation period) starts on day \t" + fertilePhaseStart + fertilePhaseEnd);
+			
 			
 		}
 
