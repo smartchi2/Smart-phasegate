0b8aae1a5e6b242d98d1da619268c5caef608bde
smartchi2 <chismart76@gmail.com>
2024-05-13 15:01:40 +0100

    update

--- phase 1/day 7/java/MenstrualApp.java
+++ phase 1/day 7/java/MenstrualApp.java
@@ -1,4 +1,32 @@
+import java.util.*;
 public class MenstrualApp{
 	public  static void main(String...App){
+	Scanner input = new Scanner(System.in);
+		System.out.printf("""
+			Welcome to Smart Female Health central..
+			we will be asking you to enter some input,
+			concerning how your menstruation starts,
+			and ends for us to be able to continue with you request..
+							""" );
+
+			int cycleLenght = 0;
+			int periodLenght = 0;
+			
+
+			System.out.println("=".repeat(100));
+			System.out.println("""
+				Please enter some info about your below menstrual
+				 cycle it Should be entered in days
+							""");
+			System.out.println("=".repeat(100));
+
+			System.out.println("""
+			Please enter the average length of your mentrual 
+			cycle that you can remember in days: """);
+			cycleLenght = input.nextInt();
+
+			//System.out.println("
+			
+		}
+
 	}
-}
