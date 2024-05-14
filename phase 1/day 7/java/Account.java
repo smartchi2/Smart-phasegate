b6a83af227f8add45704ce970a9947dfa61cd53a
smartchi2 <chismart76@gmail.com>
2024-05-14 21:03:20 +0100

    upate

--- phase 1/day 7/java/Account.java
+++ phase 1/day 7/java/Account.java
@@ -1,19 +1,21 @@
 import java.util.*;
+import java.util.Random;
 
 public class Account{
 	public static void main(String...a){
 	Scanner input = new Scanner(System.in);
 	ArrayList<String> firstNameList = new ArrayList<>();
 	ArrayList<String> lastNameList = new ArrayList<>();
+	ArrayList<String> userName1List = new ArrayList<>();
 	ArrayList<String> accountTypeList = new ArrayList<>();
 	ArrayList<Double> balanceList = new ArrayList<>();
-	ArrayList<String> bankPinList = new ArrayList<>();
+	ArrayList<String> bankPin1List = new ArrayList<>();
 	
-	
-
+	 String userName1 = null;
+	String bankPin1 = null;
 	printHeader();
 	pickOption();
-	accountType();
+	//accountType();
 	System.out.print("choose an option  ");
 	int bankOptions = input.nextInt();
 	System.out.println("=================");
@@ -28,12 +30,16 @@ public class Account{
 		System.out.print("Enter your Last Name:");
 		String  lastName = input.next();
 		lastNameList.add(lastName);
+	
+		System.out.print("Enter Prefer  User Name:");
+		userName1 = input.next();
+		userName1List.add(userName1);
 
 		System.out.println("Create your transfer pin:");
-		String  bankPin = input.nextLine();
+		String  bankPin1 = input.nextLine();
 		input.nextLine();
-		bankPinList.add(bankPin);
-		if(bankPin.length() >  6 || bankPin.length() < 6){
+		bankPin1List.add(bankPin1);
+		if(bankPin1.length() >  6 || bankPin1.length() < 6){
 		System.out.println("Please enter a valid pin, and pin must be up to 6");
 		} else {
 		System.out.println("Your transfer pin was succesfully created");
@@ -43,7 +49,30 @@ public class Account{
 		System.out.print("Invalid selection Exiting now!!!");
 		return;
 	
+	case 2:
+		System.out.println("Login.... Enter Details Below:");
+		System.out.println("User Name");
+		String userName2 = input.next();
+		if(userName2 == userName1){
+		System.out.println("Hello" + userName2);
+		}else{
+		System.out.println("User not registered with us please create account");
+		}
+		System.out.print("Enter Transfer Pin:");
+		String accountPin = input.nextLine();
+		input.nextLine();
+		if(accountPin == bankPin1){
+		System.out.println("Login Successful");
+	                  }else{
+	                 System.out.println("You have entered an invalid Bank Pin");
+		}
+			break;
+		System.out.print("Invalid selection Exiting now!!!!");
+		return;
+		
+		
 
+	
 			    }
          }
   public static void pickOption(){
@@ -65,9 +94,20 @@ public static void accountType(){
 	3. Credit
 	4. Investor Account..
 	             """;
-System.out.print(accountType);
+System.out.print(pickType);
 	
     }
+
+//public static void  generateAccountNumbers(){ 
+    //Random rnd = new Random();
+    //char [] digits = new char[11];
+   // digits[0] = (char) (random.nextInt(9) + '1');
+    //for(int index=1; index<digits.length; index++) {
+       // digits[index] = (char) (random.nextInt(10) + '0');
+   // }
+    //return Long.parseLong(new String(digits));
+	//}
+  //}
 
 public static void printHeader(){
 String header = """
